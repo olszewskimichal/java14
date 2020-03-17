0674af7312d864480658288338691150ecfad1af
Michal Olszewski <michal.a.olszewski@allegro.pl>
2020-03-17 20:50:22 +0100

    text blocks in java14

---
+++ src/main/java/text/TextBlock.java
@@ -0,0 +1,13 @@
+package text;
+
+public class TextBlock {
+	public static void main(String[] args) {
+		String lines = """
+				                "id" : 1,
+				                "name" : "productName",
+				                "price": 30.0
+				""";
+
+		System.err.println(lines);
+	}
+}
